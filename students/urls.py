5ac976d71f6cea411189363b079081be8714d758
narma1 <dito.narama@gmail.com>
2024-10-28 22:49:03 +0400

    bomboklat

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('main/', views.main_page_view, name='main_page'),
     path('students/', views.students_page_view, name='students_page'),
-]
+] + debug_toolbar_urls()
